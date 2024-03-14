f605a44fcbc0418110759ea9756ab34b329ddc2d
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2024-03-14 17:48:33 +0100

    auth connect

--- main.js
+++ main.js
@@ -1,3 +1,98 @@
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBVgNV4u6JgOFWcNsT5Zx9Cna_il49Ykdk",
+    authDomain: "cloudonly-80ade.firebaseapp.com",
+    databaseURL: "https://cloudonly-80ade-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cloudonly-80ade",
+    storageBucket: "cloudonly-80ade.appspot.com",
+    messagingSenderId: "713827963294",
+    appId: "1:713827963294:web:ba2419f1d0e9200224068f"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+const register = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res.user)
+    })
+    .catch((err) => {
+        alert(err.message)
+        console.log(err.code)
+        console.log(err.message)
+    })
+}
+
+const login = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    auth.signInWithEmailAndPassword(email, password)
+    .then((res) => {
+        console.log(res.user)
+    })
+    .catch((err) => {
+        alert(err.message)
+        console.log(err.code)
+        console.log(err.message)
+    })
+}
+
+const saveData = () => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    db.collection('users')
+    .add({
+        email: email,
+        password: password
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
+
+const readData = () => {
+    db.collection('users')
+    .get()
+    .then((data) => {
+        console.log(data.docs.map((item) => {
+            return {...item.data(), id: item.id}
+        }))
+    })
+}
+
+const updateData = () => {
+    db.collection('users').doc('6caYOiNxwviOJFIQ4Uag')
+    .update({
+        email: 'ashishisagoodboy1234@gmail.com',
+        password: '123456'
+    })
+    .then(() => {
+        alert('Data Updated')
+    })
+}
+
+const deleteData = () => {
+    db.collection('users').doc('6caYOiNxwviOJFIQ4Uag').delete()
+    .then(() => {
+        alert('Data Deleted')
+    })
+    .catch((err) =>{
+        console.log(err)
+    })
+}
+
+
+
+
+/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -45,7 +140,7 @@ setDoc(boatRef, {
 onSnapshot(boatRef, snapshot =>{
     const boat = snapshot.data();
 })
-
+*/
 
         // Rules
         // Allow read/write access on all documents to any user signed in to the application
