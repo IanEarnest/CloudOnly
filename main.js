df00f154500f8e77d0b55f3327f52104bb867f7f
Ian Harcourt-Smith <ianharcourtsmith@gmail.com>
2024-03-15 01:26:45 +0100

    result change

--- main.js
+++ main.js
@@ -14,43 +14,43 @@ const auth = firebaseApp.auth();
 const register = () => {
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
 
     auth.createUserWithEmailAndPassword(email, password)
     .then((res) => {
         console.log(res.user)
-        result = "successful registration"
+        result.innerHTML = "successful registration"
     })
     .catch((err) => {
         alert(err.message)
         console.log(err.code)
         console.log(err.message)
-        result = "failed registration"
+        result.innerHTML = "failed registration"
     })
 }
 
 const login = () => {
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
 
     auth.signInWithEmailAndPassword(email, password)
     .then((res) => {
         console.log(res.user)
-        result = "successful login"
+        result.innerHTML = "successful login"
     })
     .catch((err) => {
         alert(err.message)
         console.log(err.code)
         console.log(err.message)
-        result = "failed registration"
+        result.innerHTML = "failed registration"
     })
 }
 
 const saveData = () => {
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
 
     db.collection('users')
     .add({
@@ -59,16 +59,16 @@ const saveData = () => {
     })
     .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
-        result = "successful save"
+        result.innerHTML = "successful save"
     })
     .catch((error) => {
         console.error("Error adding document: ", error);
-        result = "failed save"
+        result.innerHTML = "failed save"
     });
 }
 
 const readData = () => {
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
 
     db.collection('users')
     .get()
@@ -76,16 +76,16 @@ const readData = () => {
         console.log(data.docs.map((item) => {
             return {...item.data(), id: item.id}
         }))
-        result = "successful read"
+        result.innerHTML = "successful read"
     })
     .catch((err) =>{
         console.log(err)
-        result = "failed read"
+        result.innerHTML = "failed read"
     })
 }
 
 const updateData = () => {
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
     
     db.collection('users').doc('j52W06qCaHWOmu4tiKYxJmphUUQ2')
     .update({
@@ -94,29 +94,53 @@ const updateData = () => {
     })
     .then(() => {
         alert('Data Updated')
-        result = "successful update"
+        result.innerHTML = "successful update"
     })
     .catch((err) =>{
         console.log(err)
-        result = "failed update"
+        result.innerHTML = "failed update"
     })
 }
 
 const deleteData = () => {
-    const result = document.getElementById('result').innerHTML
+    const result = document.getElementById('result')
 
     db.collection('users').doc('j52W06qCaHWOmu4tiKYxJmphUUQ2').delete()
     .then(() => {
         alert('Data Deleted')
-        result = "successful delete"
+        result.innerHTML = "successful delete"
     })
     .catch((err) =>{
         console.log(err)
-        result = "failed delete"
+        result.innerHTML = "failed delete"
     })
 }
 
+/*
+*/
 
+const auth2 = getAuth(firebaseApp);
+onAuthStateChanged(auth2, user => {
+    console.log("Logged in as ", user);
+});
+
+signInWithPopup(auth2, new GoogleAuthProvider())
+
+// Reference to documnet in firestore
+const db2 = getFirestore(firebaseApp);
+const boatRef = doc(db2, "boats/myboat");
+
+setDoc(boatRef, {
+    owner: auth2.currentUser.uid,
+    name: "Starfire",
+    length: 32,
+    color: "red",
+});
+
+// realtime listener
+onSnapshot(boatRef, snapshot =>{
+    const boat = snapshot.data();
+})
 
 
 /*
